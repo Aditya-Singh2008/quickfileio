1ff5df29d104d04dbd032271689cafd18f43a713
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2022-08-04 03:44:26 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include"SymmEncryption/dsymec.h"
 void readfile(char *filesname);
 
 int main(int argc, char *argv[]){
-    if(strcmp(argv[2], "-File Write") == 0){
+    if(strcmp(argv[1], "-FileWrite") == 0){
         ptrascii = (int*) malloc(sizeof(int) * (2000));
         charlen = inputlen();
         filewrite(argv[2], ptrascii, charlen);
     }
-    else if(strcmp(argv[2], "-File Read") == 0){
+    else if(strcmp(argv[1], "-FileRead") == 0){
         readfile(argv[2]);
     } else{
-        printf("-File Write or -File Read");
+        printf("-FileWrite or -FileRead");
     }
 
     return 0;
@@ -23,8 +28,9 @@ void readfile(char *filesname){
     exit(1);
   }
 
-  while(input = fgetc(readfile) != EOF){
+  while((input = fgetc(readfile)) != EOF){
     printf("%d ", input);
   }
+  printf("-1\n");
   fclose(readfile);
 }
