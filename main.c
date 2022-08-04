2dcc4360985e0b22656e3feedd3f46d0c2604e51
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2022-08-04 03:33:38 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
+void readfile(char *filesname);
 
+int main(int argc, char *argv[]){
+    if(strcmp(argv[1], "-File Write") == 0){
+        ptrascii = (int*) malloc(sizeof(int) * (2000));
+        charlen = inputlen();
+        filewrite(argv[1], ptrascii, charlen);
+    }
+    else if(strcmp(argv[1], "-File Read") == 0){
+        readfile(argv[1]);
+    } else{
+        printf("-File Write or -File Read");
+    }
+
+    return 0;
+}
+void readfile(char *filesname){
+  FILE *readfile = fopen(filesname, "r");
+  int input;
+
+  if(readfile == NULL){
+    printf("Enter a valid file name");
+    exit(1);
+  }
+
+  while(input = fgetc(readfile) != EOF){
+    printf("%d ", input);
+  }
+  fclose(readfile);
+}
