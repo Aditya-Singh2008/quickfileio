d4cba2e45b2d965bc2611084073789cbd5884565
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2022-08-04 03:34:40 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 void readfile(char *filesname);
 
 int main(int argc, char *argv[]){
-    if(strcmp(argv[1], "-File Write") == 0){
+    if(strcmp(argv[2], "-File Write") == 0){
         ptrascii = (int*) malloc(sizeof(int) * (2000));
         charlen = inputlen();
-        filewrite(argv[1], ptrascii, charlen);
+        filewrite(argv[2], ptrascii, charlen);
     }
-    else if(strcmp(argv[1], "-File Read") == 0){
-        readfile(argv[1]);
+    else if(strcmp(argv[2], "-File Read") == 0){
+        readfile(argv[2]);
     } else{
         printf("-File Write or -File Read");
     }
