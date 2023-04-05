cca3c81764825d2552eda1fa1455b6211a9f7d11
Aditya-Singh2008 <96719766+Aditya-Singh2008@users.noreply.github.com>
2023-04-05 15:42:15 -0700

    Cleared Things

--- SymmEncryption/dsymec.h
+++ SymmEncryption/dsymec.h
@@ -1,14 +1,7 @@
 int inputlen();
 int filewrite(char *filesname, int *pta, int charlen);
-void getkey();
-void shiftrow1(int *pta);
-void shiftrow2(int *pta);
-void xor1(int *pta);
-void xor2(int *pta);
+void readfile(char *filesname);
 
-int keyexpand[4] = {2, 3, 2, 3};
-char key[4] = {0};
-char keyxored[4] = {0};
 int charlen;
 int *tempascii = NULL;
 int *ptrascii = NULL;
@@ -52,84 +45,18 @@ int inputlen(){
   return sum;
 }
 
-void getkey(){
-  printf("[Enter Key]: ");
-  int keyinput;
-  int element = 0;
-  int expendno = 0;
+void readfile(char *filesname){
+  FILE *readfile = fopen(filesname, "r");
+  int input;
 
-  while((keyinput = getchar()) != '\n'){
-    key[element] = keyinput;
-    keyxored[element] = key[element] ^ keyexpand[expendno];
-    expendno++;
-    if(expendno == 4){
-      expendno = 0;
-    }
-    element++;
+  if(readfile == NULL){
+    printf("Enter a valid file name");
+    exit(1);
   }
-}
 
-void shiftrow1(int *pta){
-  int swicher;
-  int *sw1 = pta;
-  int *sw2 = (pta + 1);
-
-  swicher = *sw1;
-  *sw1 = *sw2;
-  *sw2 = swicher;
-
-  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1){
-    swicher = *sw1;
-    *sw1 = *sw2;
-    *sw2 = swicher;
+  while((input = fgetc(readfile)) != EOF){
+    printf("%d ", input);
   }
-}
-
-void shiftrow2(int *pta){
-  int swicher;
-  int *sw1 = (pta + 1);
-  int *sw2 = (pta + 2);
-
-  swicher = *sw1;
-  *sw1 = *sw2;
-  *sw2 = swicher;
-
-  while(*(sw1 += 2) != -1 && *(sw2 += 2) != -1){
-    swicher = *sw1;
-    *sw1 = *sw2;
-    *sw2 = swicher;
-  }
-}
-
-
-void xor1(int *pta){
-  int *xr = pta;
-  int keyelement = 0;
-
-  while(*xr != -1){
-    if(((*xr ^ key[keyelement]) > 31 || (*xr ^ key[keyelement]) == 10)){
-      *xr = *xr ^ key[keyelement];
-    }
-    keyelement++;
-    xr++;
-    if(keyelement == 3){
-        keyelement = 0;
-    }
-  }
-}
-
-void xor2(int *pta){
-  int *xr = pta;
-  int keyelement = 0;
-
-  while(*xr != -1){
-    if((*xr ^ keyxored[keyelement]) > 31 || (*xr ^ keyxored[keyelement]) == 10){
-      *xr = *xr ^ keyxored[keyelement];
-    }
-    keyelement++;
-    xr++;
-    if(keyelement == 3){
-        keyelement = 0;
-    }
-  }
+  printf("-1\n");
+  fclose(readfile);
 }
